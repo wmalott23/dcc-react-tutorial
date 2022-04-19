c04e5b942b79fbd6c9051670cdfec5a96d1c97e5
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-19 14:03:08 -0700

    Finished WeightTracker

--- src/Components/DisplayEntries/DisplayEntries.jsx
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -1,25 +1,25 @@
 
 const DisplayEntries = (props) => {
     return (  
-    <table>
-    <thead>
-        <tr>
-        <th>Entry Number</th>
-        <th>Weight</th>
-        <th>Date</th>
-        </tr>
-    </thead>
-    <tbody>
-        {props.parentEntries.map((entry, index) => {
-        return(
-            <tr key={index}>
-            <td>{index+1}</td>
-            <td>{entry.weight}</td>
-            <td>{entry.date}</td>
+    <table className="table">
+        <thead>
+            <tr>
+            <th>Entry Number</th>
+            <th>Weight</th>
+            <th>Date</th>
             </tr>
-        );
-        })}
-    </tbody>
+        </thead>
+        <tbody>
+            {props.parentEntries.map((entry, index) => {
+            return(
+                <tr key={index}>
+                <td>{index+1}</td>
+                <td>{entry.weight}</td>
+                <td>{entry.date}</td>
+                </tr>
+            );
+            })}
+        </tbody>
     </table>
     );
 }
