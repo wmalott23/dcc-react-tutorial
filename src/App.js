ad1bd1d139c377136f32a9b8fc4570ee9b4e78c7
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-19 13:00:24 -0700

    Added functionality to addentry

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 
 
@@ -7,9 +8,17 @@ function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}])
 
+  function addNewEntry(entry){
+
+    let tempEntries = [...entries, entry];
+
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries ={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
 
     </div>
   );
